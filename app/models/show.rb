aa33af6217b4f2720d48e12bc267fe3ba9dce17b
David Markel <davidgmarkel@gmail.com>
2018-05-02 17:07:00 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,7 +6,7 @@ class Show < ActiveRecord::Base
 
   def self.most_popular_show
     #Show.order("rating DESC").limit(1).first
-    Show.where(Show.maximum(:rating))
+    Show.where("Show.maximum(:rating)"")
   end
 
   def self.lowest_rating
